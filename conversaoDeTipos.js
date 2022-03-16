568a2bd49f5286f5b1930f837f01c32aefdff17d
Talita Dovigo <88914279+TalitaGD@users.noreply.github.com>
2022-03-16 14:13:21 -0300

    Add files via upload

---
+++ conversaoDeTipos.js
@@ -0,0 +1,11 @@
+console.log("Conversão De Tipos:")
+
+console.log("ano" + 2022);
+console.log("2" + "2");
+console.log(parseInt("2") + parseInt("2"));
+
+console.log("10" / "2");
+console.log("Talita" / 2); //NAN
+
+console.log(6.4);
+console.log(6,4);
